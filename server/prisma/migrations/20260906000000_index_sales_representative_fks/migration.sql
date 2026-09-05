9f761579353b7a306eaf985cec4e55a6a761c2ba
GoonerMAK <mashrurahsan@iut-dhaka.edu>
2026-09-06 02:45:32 +0600

    feat: add indexes for region_id, area_id, and territory_id in SalesRepresentative model

---
+++ server/prisma/migrations/20260906000000_index_sales_representative_fks/migration.sql
@@ -0,0 +1,8 @@
+-- CreateIndex
+CREATE INDEX "SalesRepresentative_region_id_idx" ON "SalesRepresentative"("region_id");
+
+-- CreateIndex
+CREATE INDEX "SalesRepresentative_area_id_idx" ON "SalesRepresentative"("area_id");
+
+-- CreateIndex
+CREATE INDEX "SalesRepresentative_territory_id_idx" ON "SalesRepresentative"("territory_id");
