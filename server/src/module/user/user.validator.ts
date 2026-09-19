9e8dc9982ce0cbba69e20a438c482b6fe1a37250
GoonerMAK <mashrurahsan@iut-dhaka.edu>
2026-09-20 02:23:16 +0600

    feat: enhance updateUserSchema to enforce password length and require at least one of password or email

--- server/src/module/user/user.validator.ts
+++ server/src/module/user/user.validator.ts
@@ -1,22 +1,28 @@
-import { z } from "zod";
+import { z } from 'zod';
 
 export const userParamsSchema = z.object({
-    id: z.uuid({ message: "Invalid ID format. Must be a UUID." })
+  id: z.uuid({ message: 'Invalid ID format. Must be a UUID.' }),
 });
 
 export const createUserSchema = z.object({
-    password: z.string().min(8, { message: "Password must be at least 8 characters long" }),
-    email: z.email({ message: "Invalid email address" }),
+  password: z.string().min(8, { message: 'Password must be at least 8 characters long' }),
+  email: z.email({ message: 'Invalid email address' }),
 });
 
 export const updateUserSchema = z.object({
-    data: z.object({
-        password: z.string().optional(),
-        email: z.email({ message: "Invalid email address" }).optional(),
+  data: z
+    .object({
+      password: z
+        .string()
+        .min(8, { message: 'Password must be at least 8 characters long' })
+        .optional(),
+      email: z.email({ message: 'Invalid email address' }).optional(),
+    })
+    .refine((data) => data.password !== undefined || data.email !== undefined, {
+      message: "At least one of 'password' or 'email' must be provided",
     }),
 });
 
-
-export type UserParams = z.infer<typeof userParamsSchema>
-export type UserCreate = z.infer<typeof createUserSchema>
-export type UserUpdate = z.infer<typeof updateUserSchema>
+export type UserParams = z.infer<typeof userParamsSchema>;
+export type UserCreate = z.infer<typeof createUserSchema>;
+export type UserUpdate = z.infer<typeof updateUserSchema>;
